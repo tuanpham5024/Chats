6226d2e536d1340a0e21d87dcda57f52522bf4bf
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-07 05:56:23 +0700

    done welcome_screen

--- lib/screens/splash/welcome_screen.dart
+++ lib/screens/splash/welcome_screen.dart
@@ -1,26 +1,45 @@
+import 'package:chats/components/default_button.dart';
 import 'package:chats/config/size_config.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatelessWidget {
   static const String routName = 'welcome_screen';
+
   @override
   Widget build(BuildContext context) {
     SizeConfig().init(context);
     return Scaffold(
+      backgroundColor: Colors.white,
       body: SafeArea(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: getProportionateScreenHeight(40),),
-              Container(
-                child: Image.asset(
-                  "assets/images/logo.png"
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(20, 0 , 10 , 20),
+          child: Container(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                SizedBox(
+                  height: getProportionateScreenHeight(40),
                 ),
-                width: getProportionateScreenHeight(120),
-              ),
-              Container(
-              )
-            ],
+                Container(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                  ),
+                  height: getProportionateScreenHeight(150),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(150),
+                ),
+                DefaultButton(
+                  text: "Đăng nhập",
+                  press: () {},
+                ),
+                SizedBox(height: 20),
+                DefaultButton(
+                  text: "Đăng ký",
+                  press: () {},
+                ),
+              ],
+            ),
           ),
         ),
       ),
