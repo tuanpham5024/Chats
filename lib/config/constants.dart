e895849181d2d585e597f8c5795c5fb19e6552d1
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-08 01:17:33 +0700

    change color backgroundUnRead

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 const kPrimaryColor = Color(0xFF2F80ED);
 const kPrimaryColorLight = Color(0xFFF4F4F4);
 
-const kBackgroundUnRead = Color(0xFFF7F7F7);
+const kBackgroundUnRead = Color(0xFFE5E5E5);
 
 const kTextPrimary = Color(0xFF1B1A57);
 const kTextSecondary = Color(0xFF4F5E7B);
