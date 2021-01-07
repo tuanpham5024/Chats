0e2c1bc7101cb2e68050ad43362aab713fd96d11
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-08 01:18:07 +0700

    add theme

--- lib/config/theme.dart
+++ lib/config/theme.dart
@@ -1,8 +1,15 @@
+import 'package:chats/config/constants.dart';
 import 'package:flutter/material.dart';
 
 ThemeData theme() {
   return ThemeData(
     scaffoldBackgroundColor: Colors.white,
+    appBarTheme: AppBarTheme(
+      color: Colors.white,
+      elevation: 0,
+      brightness: Brightness.light,
+      iconTheme: IconThemeData(color: kPrimaryColor),
+    ),
     visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
