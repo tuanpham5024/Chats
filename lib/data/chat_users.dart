d6d6c6575d7d521feb3b257d7996e7b02fdffd73
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-09 11:02:47 +0700

    create chat users

---
+++ lib/data/chat_users.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ChatUsers {
+  String text;
+  String secondaryText;
+  String image;
+  String time;
+
+  ChatUsers(
+      {@required this.text,
+      @required this.secondaryText,
+      @required this.image,
+      @required this.time});
+}
