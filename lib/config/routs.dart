ddf2d963418979bfa1121ee17b0a572b2c1bb75d
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-09 11:05:28 +0700

    add rout home_screen and chat_detail

--- lib/config/routs.dart
+++ lib/config/routs.dart
@@ -1,10 +1,15 @@
 import 'package:chats/screens/auth/login/login_screen.dart';
 import 'package:chats/screens/auth/register/register.dart';
+import 'package:chats/screens/home/chats/chat_detail.dart';
+import 'package:chats/screens/home/home_screen.dart';
 import 'package:chats/screens/splash/welcome_screen.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   WelcomeScreen.routName: (context) => WelcomeScreen(),
   LoginScreen.routName: (context) => LoginScreen(),
-  RegisterScreen.routName: (context) => RegisterScreen()
+  RegisterScreen.routName: (context) => RegisterScreen(),
+
+  HomeScreen.routName: (context) => HomeScreen(),
+  ChatDetail.routName: (context) => ChatDetail()
 };
