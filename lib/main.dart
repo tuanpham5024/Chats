bfaaa0abf0eee8b4cd5f286c7be2491b674948dc
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-09 11:05:00 +0700

    remove banner debug

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,14 @@ import 'package:flutter/material.dart';
 import 'config/routs.dart';
 import 'config/theme.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Chats',
       theme: theme(),
       initialRoute: WelcomeScreen.routName,
