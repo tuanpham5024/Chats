23ab7920d48e18826ce2380cbc81cac0d5527534
Pham Anh Tuan <tuanpham5024@gmail.com>
2021-01-11 07:16:36 +0700

    add tabs bottomNavigationBar

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:chats/screens/home/profile/profile_screen.dart';
 import 'package:flutter/material.dart';
 
+import 'calls/call_screen.dart';
 import 'chats/chat_page.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -10,28 +12,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final tabs = [
+    ChatPage(),
+    CallScreen(),
+    ProfileScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.message),
-            title: new Text('Tin nhắn'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.phone),
-            title: new Text('Cuộc gọi'),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              title: Text('Cá nhân')
-          ),
-
-        ],
-      ),
-      body: ChatPage(),
+        bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.blueAccent,
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.message),
+              title: new Text('Tin nhắn'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.phone),
+              title: new Text('Cuộc gọi'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                title: Text('Cá nhân')
+            ),
+          ],
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+        ),
+        body: tabs[_currentIndex]
     );
   }
 }
